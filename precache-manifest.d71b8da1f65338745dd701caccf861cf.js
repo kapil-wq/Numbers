5e97f37639120bfdefe4485e5ff8e61d7fa512f3
Kapil Goel <you@example.com>
2020-05-15 13:33:33 +0530

    Updates

---
+++ precache-manifest.d71b8da1f65338745dd701caccf861cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979de182a4c640e72515c888ce2efbcc",
+    "url": "/numbers/index.html"
+  },
+  {
+    "revision": "fbe9946437b827c04555",
+    "url": "/numbers/static/css/main.0fc3428f.chunk.css"
+  },
+  {
+    "revision": "a70c6bd0b615623ca981",
+    "url": "/numbers/static/js/2.a3eab99a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/numbers/static/js/2.a3eab99a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe9946437b827c04555",
+    "url": "/numbers/static/js/main.d1dc0594.chunk.js"
+  },
+  {
+    "revision": "f5dd83f6ada9293a9a3c",
+    "url": "/numbers/static/js/runtime-main.200381bd.js"
+  },
+  {
+    "revision": "c863f8028c2505f92540e0ba7c379002",
+    "url": "/numbers/static/media/PermanentMarker-Regular.c863f802.ttf"
+  }
+]);
